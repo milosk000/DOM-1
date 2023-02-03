0f512f26110c7827cd7d84f408a9d05fc4bf0ceb
ribicnikola76 <ribicnikola6@gmail.com>
2023-02-03 13:07:42 +0100

    poceli dom

---
+++ dom.js
@@ -0,0 +1,28 @@
+//console.dir(document)
+//console.log(document.domain)
+
+//console.log(document.title)
+//document.title="Moj prvi sajt"
+
+//console.dir(document.head)
+//console.log(document.all)
+//console.log(document.all[8])
+
+//document.all[8].textContent="Drugi naslov"
+
+console.log(document.forms);
+console.log(document.forms[0]);
+
+console.log(document.getElementsByClassName("list-item"));
+
+let items=document.getElementsByClassName("list-item");
+
+items[3].textContent="promenila sam listu";
+items[2].style.backgroundColor="purple";
+for(let i =0 ;i<items.length;i++){
+    if(i%2==0) items[i].style.backgroundColor="grey";
+    else items[i].style.backgroundColor="lightgrey";
+}
+
+
+
